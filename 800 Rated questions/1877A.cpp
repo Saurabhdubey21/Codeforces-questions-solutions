bff427e9df772eaca8f78f7f7aaf003b8d878c23
Saurabhdubey21 <mkd221524@gmail.com>
2026-01-21 23:20:13 +0530

    A goal of victory

---
+++ 800 Rated questions/1877A.cpp
@@ -0,0 +1,18 @@
+//A. Goals of Victory
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int sum=0;
+        for(int i=0;i<n-1;i++){
+           int ef;
+           cin>>ef;
+           sum += ef;
+        }
+        cout<<-sum<<"\n";
+    }
+}
