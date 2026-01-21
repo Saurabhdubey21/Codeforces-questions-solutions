c1e2cf99605ecf2e4174602828b1fd98517e34df
Saurabhdubey21 <mkd221524@gmail.com>
2026-01-21 23:15:52 +0530

    //2171A. Shizuku Hoshikawa and Farm Legs

---
+++ 800 Rated questions/2171A.cpp
@@ -0,0 +1,17 @@
+//2171A. Shizuku Hoshikawa and Farm Legs
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%2!=0){
+            cout<<0<<endl;
+        }
+        else{
+            cout<<n/4+1<<endl;
+        }
+    }
+}
