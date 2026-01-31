8d6607514c90c211a7f39b7c00586fd21597966e
Saurabhdubey21 <mkd221524@gmail.com>
2026-01-31 18:34:06 +0530

    A. New Year and Hurry

---
+++ 800 Rated questions/750A.cpp
@@ -0,0 +1,16 @@
+// A. New Year and Hurry
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int av_time=240-k;
+    int req_time=0;
+    int solved=0;
+    for(int i=1;i<=n;i++){
+        req_time=req_time+5*i;
+        if(req_time>av_time)break;
+        solved++;
+    }
+    cout<<solved<<endl;
+}
