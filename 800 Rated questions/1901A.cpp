28ad4ba889c9290983d93b7d18be9f4bbf606c93
Saurabhdubey21 <mkd221524@gmail.com>
2026-04-09 23:33:40 +0530

    800 and 1200 rated questions

---
+++ 800 Rated questions/1901A.cpp
@@ -0,0 +1,22 @@
+//1901A. Line Trip
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int maxi=arr[0];
+        for(int i=1;i<n;i++){
+            maxi=max(maxi,arr[i]-arr[i-1]);
+        }
+        int return_back=2*(x-arr[n-1]);
+        maxi=max(return_back,maxi);
+        cout<<maxi<<endl;
+    }
+}
