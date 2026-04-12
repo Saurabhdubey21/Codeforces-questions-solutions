76bf01dca097823516f14b40912f52fd2cddf10e
Saurabhdubey21 <mkd221524@gmail.com>
2026-04-12 17:44:11 +0530

    800 raTED QUESTIONS

---
+++ 800 Rated questions/1899A.cpp
@@ -0,0 +1,18 @@
+//1899A.Game with Integers
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    if(n%3==0)cout<<"Second"<<endl;
+    else{
+        cout<<"First"<<endl;
+    }
+}
+int main(){
+    int tc;
+    cin>>tc;
+    while(tc--){
+        solve();
+    }
+}
