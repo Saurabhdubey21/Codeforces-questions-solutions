3dbce21bd691338fd9a252ce055c2c20658c8b77
Saurabhdubey21 <mkd221524@gmail.com>
2026-06-06 00:09:45 +0530

    900 rated questions

---
+++ 900 rated questions/2140B.cpp
@@ -0,0 +1,15 @@
+//2140B. Another Divisibility Problem
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    cout<<999999999-n<<endl;
+}
+int main(){
+    int tc;
+    cin>>tc;
+    while(tc--){
+        solve();
+    }
+}
